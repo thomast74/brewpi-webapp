115947f7c1ef3dc0e0f22805dcac038c6e05a0c0
Thomas Trageser <thomast74@gmail.com>
2016-04-13 20:32:22 +0100

    get configuration shows an error 'NoneType'

--- api/helpers/Core.py
+++ api/helpers/Core.py
@@ -32,13 +32,22 @@ def prepare_configuration_dic(configuration):
     heat_actuator = configuration.get_heat_actuator()
     cool_actuator = configuration.get_cool_actuator()
     fan_actuator = configuration.get_fan_actuator()
+    pump_1_actuator = configuration.get_pump_1_actuator()
+    pump_2_actuator = configuration.get_pump_2_actuator()
 
     phases = []
     for phase in configuration.phases.all():
         phases.append({
             "start_date": phase.start_date.strftime('%Y-%m-%dT%H:%M:%SZ'),
+            "temperature": phase.temperature,
             "heat_pwm": phase.heat_pwm,
             "fan_pwm": phase.fan_pwm,
+            "pump_1_pwm": phase.pump_1_pwm,
+            "pump_2_pwm": phase.pump_2_pwm,
+            "heating_period": phase.heating_period,
+            "cooling_period": phase.cooling_period,
+            "cooling_on_time": phase.cooling_on_time,
+            "cooling_off_time": phase.cooling_off_time,
             "p": phase.p,
             "i": phase.i,
             "d": phase.d,
@@ -51,10 +60,12 @@ def prepare_configuration_dic(configuration):
         "create_date": configuration.create_date.strftime('%Y-%m-%dT%H:%M:%SZ'),
         "type": "Brew" if configuration.type == Configuration.CONFIG_TYPE_BREW else "Fermentation",
         "brewpi": configuration.brewpi.pk,
-        "temp_sensor": temp_sensor.get_function_display(),
-        "heat_actuator": heat_actuator.get_function_display(),
-        "cool_actuator": cool_actuator.get_function_display(),
-        "fan_actuator": fan_actuator.get_function_display(),
+        "fan_actuator": "" if fan_actuator is None else fan_actuator.get_function_display(),
+        "cool_actuator": "" if cool_actuator is None else cool_actuator.get_function_display(),
+        "heat_actuator": "" if heat_actuator is None else heat_actuator.get_function_display(),
+        "pump_1_actuator": "" if pump_1_actuator is None else pump_1_actuator.get_function_display(),
+        "pump_2_actuator": "" if pump_2_actuator is None else pump_2_actuator.get_function_display(),
+        "temp_sensor": "" if temp_sensor is None else temp_sensor.get_function_display(),
         "function": {
             device.get_function_display(): device.id for device in configuration.get_devices()
             },
