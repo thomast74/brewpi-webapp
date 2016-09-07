faf4adc2a6b99c168d5677d3281a498a67d760a8
Thomas Trageser <thomast74@gmail.com>
2016-09-07 23:20:17 +0100

    remove patterns requirement

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from api.views.BrewPiDetail import BrewPiDetail
 from api.views.BrewPiList import BrewPiList
@@ -13,23 +13,23 @@ from api.views.LogDetail import LogDetail
 from api.views.LogList import LogList
 
 
-urlpatterns = patterns('',
-                       url(r'^configs/(?P<device_id>\w+)/(?P<config_id>[0-9]+)/', ConfigurationDetail.as_view()),
-                       url(r'^configs/(?P<device_id>\w+)/(?P<command>\w+)/', ConfigurationCommand.as_view()),
-                       url(r'^configs/(?P<device_id>\w+)/', ConfigurationList.as_view()),
-                       url(r'^configs/', ConfigurationGeneral.as_view()),
+urlpatterns = [
+    url(r'^configs/(?P<device_id>\w+)/(?P<config_id>[0-9]+)/', ConfigurationDetail.as_view()),
+    url(r'^configs/(?P<device_id>\w+)/(?P<command>\w+)/', ConfigurationCommand.as_view()),
+    url(r'^configs/(?P<device_id>\w+)/', ConfigurationList.as_view()),
+    url(r'^configs/', ConfigurationGeneral.as_view()),
 
-                       url(r'^devices/(?P<pk>[0-9]+)/update/', DeviceDetail.as_view()),
-                       url(r'^devices/(?P<pk>[0-9]+)/delete/', DeviceDetail.as_view()),
-                       url(r'^devices/(?P<device_id>\w+)/(?P<id>[0-9]+)/', DeviceCommand.as_view()),
-                       url(r'^devices/(?P<device_id>\w+)/(?P<command>\w+)/', DeviceCommand.as_view()),
-                       url(r'^devices/(?P<device_id>\w+)/', DeviceDetail.as_view()),
-                       url(r'^devices/', DeviceList.as_view()),
+    url(r'^devices/(?P<pk>[0-9]+)/update/', DeviceDetail.as_view()),
+    url(r'^devices/(?P<pk>[0-9]+)/delete/', DeviceDetail.as_view()),
+    url(r'^devices/(?P<device_id>\w+)/(?P<id>[0-9]+)/', DeviceCommand.as_view()),
+    url(r'^devices/(?P<device_id>\w+)/(?P<command>\w+)/', DeviceCommand.as_view()),
+    url(r'^devices/(?P<device_id>\w+)/', DeviceDetail.as_view()),
+    url(r'^devices/', DeviceList.as_view()),
 
-                       url(r'^logs/(?P<device_id>\w+)/(?P<config_id>[0-9]+)/', LogDetail.as_view()),
-                       url(r'^logs/(?P<device_id>\w+)/', LogList.as_view()),
+    url(r'^logs/(?P<device_id>\w+)/(?P<config_id>[0-9]+)/', LogDetail.as_view()),
+    url(r'^logs/(?P<device_id>\w+)/', LogList.as_view()),
 
-                       url(r'^brewpis/(?P<device_id>\w+)/(?P<command>\w+)/', BrewPiDetail.as_view()),
-                       url(r'^brewpis/(?P<device_id>\w+)/', BrewPiDetail.as_view()),
-                       url(r'^brewpis/', BrewPiList.as_view()),
-                       )
+    url(r'^brewpis/(?P<device_id>\w+)/(?P<command>\w+)/', BrewPiDetail.as_view()),
+    url(r'^brewpis/(?P<device_id>\w+)/', BrewPiDetail.as_view()),
+    url(r'^brewpis/', BrewPiList.as_view()),
+]
--- oinkbrew_webapp/urls.py
+++ oinkbrew_webapp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 
@@ -7,7 +7,7 @@ handler403 = 'api.views.errors.permission_denied'
 handler404 = 'api.views.errors.page_not_found'
 handler500 = 'api.views.errors.server_error'
 
-urlpatterns = patterns('',
-                       url(r'^api/', include('api.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
-                       )
+urlpatterns = [
+    url(r'^api/', include('api.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+]
